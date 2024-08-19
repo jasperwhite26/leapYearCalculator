b7f6dc5449e3265b6e6fc74b4b8ea5662fbf76e1
jasper <whiteana001@warren.k12.in.us>
2024-08-19 13:40:02 -0400

    initial commit

--- src/leapYearCalculator.java
+++ src/leapYearCalculator.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class leapYearCalculator {
+public class leapYearCalculator {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+    }
+
+    private static boolean isLeapYear(int year) {
+        return div4Check(year);
+
+    }
+
+    private static boolean div4Check(int year) {
+        if (year % 4 == 0) {
+            return div100Check(year);
+
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean div100Check(int year) {
+        if (year % 100 == 0){
+            return div400Check(year);
+        } else{
+            return true;
+        }
+    }
+
+    private static boolean div400Check(int year) {
+        return year % 400 == 0;
+    }
 }
